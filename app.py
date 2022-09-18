e945d57d7d6c2ef57b00e4a18950b4f4ccad2b71
ankushkamra <113813859+ankushkamra@users.noreply.github.com>
2022-09-18 11:33:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('SalaryPredictor.mdl', 'rb'))
+model = pickle.load(open('salaryprediction.mdl', 'rb'))
 
 @app.route('/')
 def home():
